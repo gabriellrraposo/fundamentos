a892740e26c2d63cebfe4bd1227277aea6caf26d
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2023-05-13 19:30:26 -0300

    Utilzando elementos filhos por meio de props de componentes

---
+++ pages/examples/filhos.tsx
@@ -0,0 +1,12 @@
+import Grade from "@/components/examples/Grade";
+import Caixa from "@/components/examples/Caixa";
+
+export default function PaginaFilhos() {
+    return (
+        <Grade>
+            <Caixa>1</Caixa>
+            <Caixa>2</Caixa>
+            <Caixa>3</Caixa>
+        </Grade>
+    )
+}
