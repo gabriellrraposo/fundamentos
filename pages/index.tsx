c9f976f65e7ef9c88c82692378b6f00d09758866
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2023-05-13 17:48:39 -0300

    Adicionando os primeiros conteúdos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
-import { Icon24Hours } from "@tabler/icons-react"
+import { Icon24Hours } from "@tabler/icons-react";
 
 export default function Home() {
   return (
-    <div className={`
+    <div
+      className={`
       flex justify-center items-center h-screen
-    bg-black text-white text-6xl`}>
-      <Icon24Hours size={100} strokeWidth={1.3}/>
-      <span>
-        Fundamentos
-      </span>
+      bg-black text-white text-6xl`}>
+      
+      <Icon24Hours size={100} strokeWidth={1.3} />
+      <span>Fundamentos</span>
     </div>
-  )
+  );
 }
