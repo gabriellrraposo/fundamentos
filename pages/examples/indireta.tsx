3c088a04875a447d30628caf9ff3861d0f7209cf
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2023-05-14 10:28:29 -0300

    Exemplo de comunicação indireta

---
+++ pages/examples/indireta.tsx
@@ -0,0 +1,13 @@
+
+import Pagina from "@/components/template/Pagina";
+import Painel from "@/components/examples/indireta/Painel";
+
+
+
+export default function PaginaDireta() {
+    return (
+        <Pagina>
+            <Painel />
+        </Pagina>
+    )
+}
